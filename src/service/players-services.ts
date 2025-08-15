23ab84445f22f85199a243a823d77ffa921cbe8e
Matheus Araujo <theusnascimento386@gmail.com>
2025-08-15 20:51:07 -0300

    rafactor: app, service and utilities

--- src/service/players-services.ts
+++ src/service/players-services.ts
@@ -1,8 +1,9 @@
+import * as findAllPlayers from './../repositories/players-repository';
 import { noContent, ok } from "../utils/hhtp-helper"
 
 
 export const getPlayerService = async () => {
-    const data = { player: "Matheus" }
+    const data = await findAllPlayers.findAllPlayers()
     let response = null
 
     if (data) {
