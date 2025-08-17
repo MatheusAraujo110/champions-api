14ab5b0abace5d3458b2cbf374e5f41b3897380a
Matheus Araujo <theusnascimento386@gmail.com>
2025-08-16 21:18:55 -0300

    feat: implemented cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import router from "./routes"
 
 function createApp() {
@@ -7,6 +8,11 @@ function createApp() {
     app.use(express.json())
     app.use("/api", router)
 
+    // const corsOptions = {
+    //     origin: ['http://matheus.system.com', 'www.gov.br'],  // Ex: mais de uma autorização e add a URL. delemita quem pode usar a minha API
+    //     methods: ['GET', 'UPDATE']
+    // }
+    app.use(cors()) // dentro de cors(), só pode ser chamado que for www.matheus...
 
     return app
 }
